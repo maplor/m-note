a455f719d9bbc7650da9fcfad9604240bdc046b7
maplor <maplor@163.com>
2024-01-02 00:17:24 +0800

    refactor: store 包装返回格式，优化错误处理逻辑

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,40 +1,70 @@
 'use client';
 
+import { ReactNode } from 'react';
 import localforage from 'localforage';
 import { Toast } from 'antd-mobile';
 import { FrownOutline } from 'antd-mobile-icons';
 import type { NoteItem } from '@/type';
 
 const LOCAL_KEY = 'm-note-list';
+const MAX_SAVE_LENGTH = 1000;
+const tooMuchTip = <p style={{ textAlign: 'center' }}>记录太多啦！<br />请先删除部分笔记</p>;
 
-function showError() {
+function showError(msg?: ReactNode) {
   Toast.show({
-    content: '后台开小差了',
+    content: msg || '后台开小差了',
     icon: <FrownOutline />,
   });
 }
+
+function handleError() {
+  showError();
+  return {
+    success: false,
+  };
+}
+
+type StoreResult<T = any> = {
+  success: boolean;
+  data?: T;
+};
 
 /**
  * 使用本地存储模拟接口，直接返回对应的数据格式
  * */
 const store = {
-  getAll() {
+  getAll(): Promise<StoreResult<NoteItem[]>> {
     return localforage.getItem<NoteItem[]>(LOCAL_KEY)
       .then(function(list) {
-        return list || [];
+        return {
+          success: true,
+          data: list || [],
+        };
       })
-      .catch(showError);
+      .catch(handleError);
   },
-  add(newItem: NoteItem) {
+  add(newItem: NoteItem): Promise<StoreResult<undefined>> {
     return localforage.getItem<NoteItem[]>(LOCAL_KEY)
       .then(function(list) {
         const currentList = list || [];
 
-        return localforage.setItem(LOCAL_KEY, [newItem, ...currentList]);
+        if (currentList.length >= MAX_SAVE_LENGTH) {
+          showError(tooMuchTip);
+          return {
+            success: false,
+          };
+        }
+
+        return localforage.setItem(LOCAL_KEY, [newItem, ...currentList])
+          .then(() => {
+            return {
+              success: true,
+            };
+          });
       })
-      .catch(showError);
+      .catch(handleError);
   },
-  deleteById(id: string) {
+  deleteById(id: string): Promise<StoreResult<undefined>> {
     return localforage.getItem<NoteItem[]>(LOCAL_KEY)
       .then(function(list) {
         const currentList = list || [];
@@ -43,11 +73,16 @@ const store = {
           return item.id !== id;
         });
 
-        return localforage.setItem(LOCAL_KEY, newList);
+        return localforage.setItem(LOCAL_KEY, newList)
+          .then(() => {
+            return {
+              success: true,
+            };
+          });
       })
-      .catch(showError);
+      .catch(handleError);
   },
-  setFinishById(id: string, finish: boolean) {
+  setFinishById(id: string, finish: boolean): Promise<StoreResult<undefined>> {
     return localforage.getItem<NoteItem[]>(LOCAL_KEY)
       .then(function(list) {
         const currentList = list || [];
@@ -63,9 +98,14 @@ const store = {
           };
         });
 
-        return localforage.setItem(LOCAL_KEY, newList);
+        return localforage.setItem(LOCAL_KEY, newList)
+          .then(() => {
+            return {
+              success: true,
+            };
+          });
       })
-      .catch(showError);
+      .catch(handleError);
   },
 };
 
